f143505c027e9a9c30a0cd4447b32da5a85ba0bb
retchut <retchut@protonmail.ch>
2021-03-11 20:39:41 +0000

    feat: Implement Card class getter and setter methods

--- src/Card.java
+++ src/Card.java
@@ -3,28 +3,44 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Card {
-    String name, expansion, condition, language;
-    boolean firstEd;
-    int amount;
+    private String name, expansion, condition, language;
+    private boolean firstEd;
+    private int amount;
+
+    //Getters
+    public String getName(){ return this.name; }
+    public String getExpansion(){ return this.expansion; }
+    public String getCondition(){ return this.name; }
+    public String getLanguage(){ return this.language; }
+    public boolean getFirstEd(){ return this.firstEd; }
+    public int getAmount(){ return this.amount; }
+
+    //setters
+    public void setName(String newName){ this.name = newName; }
+    public void setExpansion(String newExpansion){ this.expansion = newExpansion; }
+    public void setCondition(String newCondition){ this.condition = newCondition; }
+    public void setLanguage(String newLanguage){ this.language = newLanguage; }
+    public void setFirstEd(boolean newFirstEd){ this.firstEd = newFirstEd; }
+    public void setAmount(int newAmount) { this.amount = newAmount; }
 
     //Class constructor
     public Card(String name, String expansion, String condition, String language, boolean firstEd, int amount){
-        this.name = name;
-        this.expansion = expansion;
-        this.condition = condition;
-        this.language = language;
-        this.firstEd = firstEd;
-        this.amount = amount;
+        setName(name);
+        setExpansion(expansion);
+        setCondition(condition);
+        setLanguage(language);
+        setFirstEd(firstEd);
+        setAmount(amount);
     }
 
     private void printCard(){
         //clearConsole()
-        System.out.println("Name: " + this.name);
-        System.out.println("Expansion: " + this.expansion);
-        System.out.println("Condition: " + this.condition);
-        System.out.println("Language: " + this.language);
-        System.out.println("Is First Ed: " + this.firstEd);
-        System.out.println("Amount: " + this.amount);
+        System.out.println("Name: " + getName());
+        System.out.println("Expansion: " + getExpansion());
+        System.out.println("Condition: " + getCondition());
+        System.out.println("Language: " + getLanguage());
+        System.out.println("Is First Ed: " + getFirstEd());
+        System.out.println("Amount: " + getAmount());
     }
 
     private void addCopies(){
@@ -41,9 +57,9 @@ public class Card {
                     return;
                 }
                 else{
-                    this.amount += toAdd;
-                    System.out.println("You added " + toAdd + " copies of " + this.expansion + " " + this.name + "to your library.");
-                    System.out.println("You now own " + this.amount + " copies of this card.");
+                    setAmount(getAmount() + toAdd);
+                    System.out.println("You added " + toAdd + " copies of " + getExpansion() + " " + getName() + "to your library.");
+                    System.out.println("You now own " + getAmount() + " copies of this card.");
                 }
             }
             catch(InputMismatchException e){
@@ -66,15 +82,15 @@ public class Card {
                     System.out.println("That is an invalid amount. No changes were made.");
                     return;
                 }
-                else if(toRemove > this.amount){
+                else if(toRemove > getAmount()){
                     System.out.println("That amount is larger than the copies of this card you currently own.");
-                    System.out.println("You currently own " + this.amount + " copies of " + this.expansion + " " + this.name + ".");
+                    System.out.println("You currently own " + getAmount() + " copies of " + getExpansion() + " " + getName() + ".");
                     continue;
                 }
                 else{
-                    this.amount += toRemove;
-                    System.out.println("You removed " + toRemove + " copies of " + this.expansion + " " + this.name + "to your library.");
-                    System.out.println("You now own " + this.amount + " copies of this card.");
+                    setAmount(getAmount() - toRemove);
+                    System.out.println("You removed " + toRemove + " copies of " + getExpansion() + " " + getName() + "to your library.");
+                    System.out.println("You now own " + getAmount() + " copies of this card.");
                 }
             }
             catch(InputMismatchException e){
