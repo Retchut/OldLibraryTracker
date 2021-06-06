e09fdb41b8383f2bc67307fd445105eb7699556a
Mário Travassos <retchut@protonmail.ch>
2021-06-06 20:20:44 +0100

    fix: comma bug

--- src/main/java/com/retchut/LibraryTracker/Model/UrlBuilder.java
+++ src/main/java/com/retchut/LibraryTracker/Model/UrlBuilder.java
@@ -1,11 +1,15 @@
 package com.retchut.LibraryTracker.Model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 final public class UrlBuilder {
     private StringBuilder url;
+    List<Character> replaceWithDash;
 
     private void getUrlExpansion(String expansionKey){
         url.append(ExpansionMap.getExpansion(expansionKey));
-        System.out.println(ExpansionMap.getExpansion(expansionKey));
         url.append('/');
     }
 
@@ -15,9 +19,10 @@ final public class UrlBuilder {
             if(current == '"'){
                 //ignore
             }
-            else if(current == ' '){
-                //replace space with -
-                url.append('-');
+            else if(replaceWithDash.contains(current)){
+                //replace space with '-', or write '-', if one hasn't been already written
+                if(url.charAt(url.length()-1) != '-')
+                    url.append('-');
             }
             else{
                 //add normal char to url
@@ -28,6 +33,7 @@ final public class UrlBuilder {
     }
     public UrlBuilder(){
         this.url = new StringBuilder();
+        this.replaceWithDash = Arrays.asList(' ', '-', ',', '?', '!');
     }
 
 
